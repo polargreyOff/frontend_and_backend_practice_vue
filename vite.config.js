1fc1a9cdd822ab9c85b655bcf9279203c24b8efe
polargreyOff <spotmaster2017@yandex.ru>
2025-12-09 20:38:30 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' 
+    ? '/frontend_and_backend_practice_vue/' 
+    : '/',
+  plugins: [vue()]
 })
